575d3620bf36c2d4a2a3520267d9de4b0e0aa1db
Kostas Vs <kostasvs@live.com>
2022-07-04 00:09:35 +0300

    add homicides/year and homicides/country

--- js/scripts.js
+++ js/scripts.js
@@ -12,6 +12,11 @@ $(document).ready(function () {
 				speed: 1000,
 			},
 		},
+		legend: {
+			showForSingleSeries: true,
+			position: 'top',
+			horizontalAlign: 'right',
+		},
 		stroke: {
 			width: 3,
 		},
@@ -31,10 +36,11 @@ $(document).ready(function () {
 		}
 	};
 
+	// Chart 1
 	var chart1;
 	var chart1data;
 	var f1type = 0, f1race = 5;
-	$.get("assets/incident-counts-age-race-category.csv", function (data) {
+	$.get("assets/us-incident-counts-age-race-category.csv", function (data) {
 
 		chart1data = $.csv.toObjects(data);
 
@@ -43,11 +49,6 @@ $(document).ready(function () {
 				type: 'line'
 			},
 			series: seriesChart1(),
-			legend: {
-				showForSingleSeries: true,
-				position: 'top',
-				horizontalAlign: 'right',
-			},
 			xaxis: {
 				title: {
 					text: "Age (years)"
@@ -116,18 +117,175 @@ $(document).ready(function () {
 		}];
 	}
 
+	// Chart 2
+	var chart2;
+	var chart2data;
+	var f2gender = 'Combined';
+	$.get("assets/total_homicides_year.csv", function (data) {
+
+		chart2data = $.csv.toObjects(data);
+
+		var options = {
+			chart: {
+				type: 'line'
+			},
+			series: seriesChart2(),
+			xaxis: {
+				title: {
+					text: "Year"
+				},
+				type: 'numeric',
+				decimalsInFloat: 0,
+			},
+		}
+
+		chart2 = new ApexCharts(document.querySelector("#chart-homicides-year"), options);
+		chart2.render();
+
+		$('#filters-homicides-year').fadeIn().find('.btn-check').each(function () {
+			$(this).click(clickF2Option);
+		});
+	});
+
+	function clickF2Option() {
+
+		var optName = $(this).attr('name');
+		var optVal = $(this).val();
+
+		if (optName == 'f2gender') {
+			f2gender = optVal;
+		}
+
+		chart2.updateSeries(seriesChart2());
+	}
+
+	function seriesChart2() {
+
+		if (f2gender == 'Combined') {
+			var categories = ['Total', 'Male', 'Female', 'Unknown'];
+			var series = [];
+			for (const element of categories) {
+
+				series.push({
+					name: element,
+					data: chart2data.filter(x => x.Sex == element).map(x => [+x.Year, +x.victims])
+				});
+			}
+			return series;
+		}
+
+		return [{
+			name: 'Homicides',
+			data: chart2data.filter(x => x.Sex == f2gender).map(x => [+x.Year, +x.victims])
+		}];
+	}
+
+	// google geochart
+	var chart3;
+	var chart3data;
+	var f3start = 1990;
+	var f3end = 2020;
+	var f3gender = 'Total';
+	var f3measure = 'N';
+	var mapOptions = {
+		backgroundColor: 'transparent',
+		colorAxis: { colors: ['#F5F5F5', '#e31b23'] },
+	};
+
+	google.charts.load('current', {
+		'packages': ['geochart'],
+	});
+	google.charts.setOnLoadCallback(drawRegionsMap);
+
+	function drawRegionsMap() {
+
+		$.get("assets/homicides_country_year.csv", function (data) {
+
+			chart3data = $.csv.toObjects(data, { separator: ';' });
+
+			let filters = $('#filters-homicides-country');
+			filters.fadeIn().find('.btn-check').each(function () {
+				$(this).click(clickF3Option);
+			});
+			filters.find('.form-range').each(function () {
+				$(this).change(clickF3Option);
+			});
+
+			var data3 = seriesChart3();
+			chart3 = new google.visualization.GeoChart(document.getElementById('chart-homicides-country'));
+			chart3.draw(data3, mapOptions);
+		});
+	}
+
+	function clickF3Option() {
+
+		var optName = $(this).attr('name');
+		var optVal = $(this).val();
+
+		switch (optName) {
+			case 'f3start':
+				f3start = +optVal;
+				if (f3end < f3start) {
+					$('#f3end').val(f3start);
+				}
+				break;
+
+			case 'f3end':
+				f3end = +optVal;
+				if (f3end < f3start) {
+					$('#f3start').val(f3end);
+				}
+				break;
+
+			case 'f3gender':
+				f3gender = optVal;
+				break;
+
+			case 'f3measure':
+				f3measure = optVal;
+				break;
+
+			default:
+				return;
+		}
+
+		f3start = $('#f3start').val();
+		f3end = $('#f3end').val();
+		$('#f3starttext').text(f3start);
+		$('#f3endtext').text(f3end);
+		chart3.draw(seriesChart3(), mapOptions);
+	}
+
+	function seriesChart3() {
+
+		var arr;
+		var countries = [...new Set(chart3data.map(x => x.Country))];
+		countries = countries.map(x => [x, 0]);
+		arr = chart3data.filter(x => x.Sex == f3gender &&
+			+x.Year >= f3start && +x.Year <= f3end &&
+			x.Unit == f3measure)
+			.map(x => [x.Country, +x.VALUE]);
+		arr.forEach(function (item) {
+			var i = countries.findIndex(function (c) {
+				return c[0] == item[0];
+			})
+			if (i != -1) countries[i][1] += item[1];
+		});
+		arr = countries;
+		arr.unshift(['Country', f3measure == 'N' ? 'Homicides' : 'Homicides per 100,000']);
+		return google.visualization.arrayToDataTable(arr);
+	}
+
+	// show/hide while scrolling
 	const boxes = document.querySelectorAll('.scrollytellingBox');
 	window.addEventListener('scroll', checkBoxes);
 	checkBoxes();
 	function checkBoxes() {
-		const triggerBottom = window.innerHeight * 0.7;
+		const triggerBottom = window.innerHeight * 0.8;
 		boxes.forEach(box => {
 			const boxTop = box.getBoundingClientRect().top;
 			if (boxTop < triggerBottom) {
 				box.classList.add('show');
-			}
-			else {
-				box.classList.remove('show');
 			}
 		})
 	}
